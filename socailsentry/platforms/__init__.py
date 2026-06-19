c479c4e6ed344724f4a3ed4b1d1988373fdf930c
sonia065 <sonilaique165@gmail.com>
2026-06-19 11:30:33 +0400

    Update __init__.py

--- socailsentry/platforms/__init__.py
+++ socailsentry/platforms/__init__.py
@@ -1,1 +1,5 @@
-
+from .facebook import FacebookScanner
+from .instagram import InstagramScanner
+from .youtube import YouTubeScanner
+from .tiktok import TikTokScanner
+from .whatsapp import WhatsAppScanner
